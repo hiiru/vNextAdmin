76589afa7da87dd9b53410ee485656c25f4f2267
Hiiru <hiiru@rulez.ch>
2014-11-03 22:04:18 +0100

    added missing files

---
+++ src/vNextAdmin/Components/PageItems/FormComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Mvc;
+using System.Threading.Tasks;
+using System;
+using vNextAdminLib.Components;
+using vNextAdminLib.Components.Dashboard;
+
+namespace vNextAdmin.Components
+{
+    [ViewComponent(Name = "Form")]
+    public class FormComponent : ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync(IAdminPageItem item)
+        {
+            if (item != null)
+                return View(item.ViewComponentMethod, item);
+            return Content("");
+        }
+    }
+}
---
+++ src/vNextAdminLib/Components/Common/Alert.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vNextAdminLib.Components.Common
+{
+    public class Alert : AAdminPageItem
+    {
+        public override string ViewComponentIdentifier { get { return "common"; } }
+        public override string ViewComponentMethod { get { return "alert"; } }
+        
+        public bool Dismissable { get; set; }
+        public string Text { get; set; }
+        public AlertStatus Status { get; set; }
+        public bool IsHtmlText { get; set; }
+
+        public string GetCssClasses()
+        {
+            var sb = new StringBuilder("alert alert-");
+            switch (Status)
+            {
+                case AlertStatus.Danger:
+                    sb.Append("danger");
+                    break;
+                case AlertStatus.Warning:
+                    sb.Append("warning");
+                    break;
+                case AlertStatus.Success:
+                    sb.Append("success");
+                    break;
+                case AlertStatus.Info:
+                    sb.Append("info");
+                    break;
+            }
+            if (Dismissable)
+                sb.Append(" alert-dismissable");
+            return sb.ToString();
+        }
+    }
+    public enum AlertStatus
+    {
+        Info,
+        Success,
+        Warning,
+        Danger
+    }
+}
